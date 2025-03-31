eef819b54d93fc7924e9d037d3080afd66e16bdd
adugbo aghogho <adugboaghogho@gmail.com>
2025-03-31 19:24:53 +0100

    sho

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -83,7 +83,8 @@ const PostForm = ({ post, action }: PostFormProps) => {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(handleSubmit)}
-        className="flex flex-col gap-9 w-full  max-w-5xl">
+        className="flex flex-col gap-9 w-full  max-w-5xl"
+      >
         <FormField
           control={form.control}
           name="caption"
@@ -92,7 +93,7 @@ const PostForm = ({ post, action }: PostFormProps) => {
               <FormLabel className="shad-form_label">Caption</FormLabel>
               <FormControl>
                 <Textarea
-                  className="shad-textarea custom-scrollbar"
+                  className="shad-textarea bg-dark-3 rounded-[1rem] custom-scrollbar"
                   {...field}
                 />
               </FormControl>
@@ -125,7 +126,11 @@ const PostForm = ({ post, action }: PostFormProps) => {
             <FormItem>
               <FormLabel className="shad-form_label">Add Location</FormLabel>
               <FormControl>
-                <Input type="text" className="shad-input" {...field} />
+                <Input
+                  type="text"
+                  className="shad-input bg-dark-4"
+                  {...field}
+                />
               </FormControl>
               <FormMessage className="shad-form_message" />
             </FormItem>
@@ -144,7 +149,7 @@ const PostForm = ({ post, action }: PostFormProps) => {
                 <Input
                   placeholder="Art, Expression, Learn"
                   type="text"
-                  className="shad-input"
+                  className="shad-input bg-dark-4"
                   {...field}
                 />
               </FormControl>
@@ -157,13 +162,15 @@ const PostForm = ({ post, action }: PostFormProps) => {
           <Button
             type="button"
             className="shad-button_dark_4"
-            onClick={() => navigate(-1)}>
+            onClick={() => navigate(-1)}
+          >
             Cancel
           </Button>
           <Button
             type="submit"
             className="shad-button_primary whitespace-nowrap"
-            disabled={isLoadingCreate || isLoadingUpdate}>
+            disabled={isLoadingCreate || isLoadingUpdate}
+          >
             {(isLoadingCreate || isLoadingUpdate) && <Loader />}
             {action} Post
           </Button>
