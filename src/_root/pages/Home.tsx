929cd0e75c04e5914e255fbbf6046919aea22e96
adugbo aghogho <adugboaghogho@gmail.com>
2025-04-03 13:30:25 +0100

    posts are showing

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,42 +1,42 @@
-// import { Models } from "appwrite";
+import { Models } from "appwrite";
 
 import { useToast } from "../../components/ui/use-toast";
 import { Loader, PostCard, UserCard } from "../../components/shared";
-// import { useGetRecentPosts, useGetUsers } from "../../lib/react-query/queries";
+import { useGetRecentPosts, useGetUsers } from "../../lib/react-query/queries";
 
 const Home = () => {
   const { toast } = useToast();
 
-  // const {
-  //   data: posts,
-  //   isLoading: isPostLoading,
-  //   isError: isErrorPosts,
-  // } = useGetRecentPosts();
-  // const {
-  //   data: creators,
-  //   isLoading: isUserLoading,
-  //   isError: isErrorCreators,
-  // } = useGetUsers(10);
+  const {
+    data: posts,
+    isPending: isPostLoading,
+    isError: isErrorPosts,
+  } = useGetRecentPosts();
+  const {
+    data: creators,
+    isLoading: isUserLoading,
+    isError: isErrorCreators,
+  } = useGetUsers(10);
 
-  // if (isErrorPosts || isErrorCreators) {
-  //   return (
-  //     <div className="flex flex-1">
-  //       <div className="home-container custom-scrollbar">
-  //         <p className="body-medium text-[#FFFFFF]">Something bad happened</p>
-  //       </div>
-  //       <div className="home-creators  custom-scrollbar">
-  //         <p className="body-medium text-[#FFFFFF]">Something bad happened</p>
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  if (isErrorPosts || isErrorCreators) {
+    return (
+      <div className="flex flex-1">
+        <div className="home-container custom-scrollbar">
+          <p className="body-medium text-[#FFFFFF]">Something bad happened</p>
+        </div>
+        <div className="home-creators  custom-scrollbar">
+          <p className="body-medium text-[#FFFFFF]">Something bad happened</p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-1">
       <div className="home-container custom-scrollbar">
         <div className="home-posts">
           <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
-          {/* {isPostLoading && !posts ? (
+          {isPostLoading && !posts ? (
             <Loader />
           ) : (
             <ul className="flex flex-col flex-1 gap-9 w-full ">
@@ -46,13 +46,13 @@ const Home = () => {
                 </li>
               ))}
             </ul>
-          )} */}
+          )}
         </div>
       </div>
 
       <div className="home-creators">
         <h3 className="h3-bold text-[#FFFFFF]">Top Creators</h3>
-        {/* {isUserLoading && !creators ? (
+        {isUserLoading && !creators ? (
           <Loader />
         ) : (
           <ul className="grid 2xl:grid-cols-2 gap-6">
@@ -62,7 +62,7 @@ const Home = () => {
               </li>
             ))}
           </ul>
-        )} */}
+        )}
       </div>
     </div>
   );
