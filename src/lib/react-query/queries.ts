adab25030aa5043e030c54f3df3adef5e97bca37
adugbo aghogho <adugboaghogho@gmail.com>
2025-05-09 14:12:38 +0100

    try 2

--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -59,13 +59,12 @@ export const useGetPosts = () => {
   return useInfiniteQuery({
     queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
     queryFn: getInfinitePosts as any,
+    initialPageParam: null, // ✅ Add this line
     getNextPageParam: (lastPage: any) => {
-      // If there's no data, there are no more pages.
-      if (lastPage && lastPage.documents.length === 0) {
+      if (!lastPage || lastPage.documents.length === 0) {
         return null;
       }
 
-      // Use the $id of the last document as the cursor.
       const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
       return lastId;
     },
