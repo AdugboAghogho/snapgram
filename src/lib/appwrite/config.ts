0c8e4b9e3fe8f3cd13878202a7db0af72781dadb
adugbo aghogho <adugboaghogho@gmail.com>
2025-05-14 16:37:08 +0100

    joel

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -4,10 +4,10 @@ export const appwriteConfig = {
   projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
   url: import.meta.env.VITE_APPWRITE_URL,
   databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID || '67d404770027c4b2c27e',
-  storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
-  userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
-  postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
-  savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
+  storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID || ' 67d40410001e71d98f78',
+  userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID || '67d495340031dc3c6b21',
+  postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID || '67d48473002d70939676',
+  savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID || '67d49571002d15df2fcd',
 };
 
 export const client = new Client();
